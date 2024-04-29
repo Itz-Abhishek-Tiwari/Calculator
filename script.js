a7fa04d8b12968cbe5f89e160d72338670e475e8
Abhishek Tiwari <69257109+Itz-Abhishek-Tiwari@users.noreply.github.com>
2024-04-29 22:47:21 +0530

    made an simple ui for calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+let numberOne = 3;
+let oper = "+";
+let numberTwo = 5;
+
+function operator(numOne, oper, numTwo) {
+  add(numOne, oper, numTwo);
+}
+
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+
+function divide() {}
